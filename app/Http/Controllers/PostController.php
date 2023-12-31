c8993a7de3fab4e879c027c7277f0c242a5605de
aahilalwani25 <alwani.aahil25@gmail.com>
2023-12-31 22:23:37 +0500

    modified ORM of Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,9 +28,9 @@ class PostController extends Controller
             $post->description = $request->description;
             $post->privacy = $privacy_levels;
             $post->is_allow = ($is_allow) ? $is_allow : $is_deny;
-            // $post->is_reel = $request->is_reel; // one to one hierarchy
+            //$post->is_reel = $request->is_reel; // one to one hierarchy (uncommented)
             if ($post->save() && $request->hasFile("image")) {
-                # code...
+                # code...   
                 $media = MediaController::uploadImage($request->file("image"), $post->id);
                 if (MediaController::saveMedia($media, $post->id, $request->is_reel)) {
                     return new JsonResponse([
@@ -56,15 +56,43 @@ class PostController extends Controller
     public function fetch_post(Request $request)
     {
 
-        $post = Post::select()
-            ->Join('media', 'post_id_fk', '=', 'post.id')
-            ->Join('profile','profile.id','=','post.profile_id_fk')
-            ->where([
-                'is_allow' => true,
-                'media.is_reel' => $request->is_reel
-            ])
-            ->orderByDesc('post.created_at')
-            ->limit(4)
+        // $post = Post::select()
+        //     ->Join('media', 'post_id_fk', '=', 'post.id')
+        //     ->Join('profile','profile.id','=','post.profile_id_fk')
+        //     ->where([
+        //         'is_allow' => true,
+        //         'media.is_reel' => $request->is_reel
+        //     ])
+        //     ->orderByDesc('post.created_at')
+        //     ->limit(4)
+        //     ->get();
+
+            $post = Post::select(
+                'post.id',
+                'media.post_id_fk',
+                'post.profile_id_fk',
+                'post.description',
+                'post.created_at',
+                'post.updated_at',
+                'post.is_reel',
+                'media.extention',
+                'media.url',
+                'profile.username',
+                'profile.profile_img_url',
+                'media.id as media_image_id',
+                'post_reaction.reaction_type_id_fk'
+            )
+            ->join('media', 'post.id', '=', 'media.post_id_fk')
+            ->join('profile', 'profile.id', '=', 'post.profile_id_fk')
+            ->leftJoin('post_reaction', function ($join) {
+                $join->on('post.id', '=', 'post_reaction.post_id_fk')
+                    ->where('post_reaction.profile_id_fk', 2);
+            })
+            ->where('post.is_allow', true)
+            ->where('post.privacy', 1)
+            ->where('media.is_reel', $request->is_reel)
+            ->orderBy('post.id', 'desc')
+            ->limit(10)
             ->get();
 // dd($post->toSql());
 //         if($post->id){
